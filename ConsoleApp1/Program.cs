cacf8e79b6087715aa87c97aa01b339fbd974688
harsh <harsh@DESKTOP-2GQ2PKH>
2023-10-12 22:04:12 +0530

    Code

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using Aspose.Pdf;
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        using (Document pdfDocument = new Document())
+        {
+            int i = 1;
+            foreach(var n in Directory.GetFiles(@"C:\Users\harsh\Desktop\Logs\Images"))
+            {
+            pdfDocument.Pages.Add();
+                Aspose.Pdf.Image image = new Aspose.Pdf.Image();
+
+                // Load sample BMP image file
+                image.File = n;
+                pdfDocument.Pages[i].Paragraphs.Add(image);
+                i++;
+            }
+            // Save output PDF document
+            pdfDocument.Save(@"C:\Users\harsh\Desktop\Logs\Images\marge.pdf");
+        }
+
+
+    }
+}
